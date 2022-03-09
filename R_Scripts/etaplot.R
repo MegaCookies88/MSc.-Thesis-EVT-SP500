736110935062693d946df0c45766382f49f32151
MegaCookies88 <nouira.mongi@gmail.com>
2022-03-09 17:27:04 +0100

    Create etaplot.R

---
+++ R_Scripts/etaplot.R
@@ -0,0 +1,87 @@
+library("evd")
+
+# Color Map
+c1 = "#636EFA"
+c2 = "#EF553B"
+
+# Data
+df = read.csv("~/Documents/GitHub/PDM_2022/data/data1.csv")
+ll = -100*sapply(df[seq(2,12)], function(x) log(x[-1])-log(x[-length(df$Date)]))
+ll = data.frame(ll)
+ll$Date = df$Date[-1]
+
+# Sectors
+sectors = sapply(colnames(ll)[1:11], function(x) substring(x,5))
+sectors = as.vector(sectors)
+
+# Output Parameters
+size = 3000
+
+# Plot All Pairs
+jpeg(
+  file = "~/Documents/GitHub/PDM_2022/figures/etaplot.jpeg",
+  width = size, height = size, quality = 100, res = 100
+)
+
+par(mfrow=c(11,11), pty="s", mar=c(0,0,0,0), oma=c(4,4,0,0))
+
+for (i in seq(1,11)){
+  for (j in seq(1,11)){
+    
+    if (j>i){
+      plot.new()
+    } else{
+      if (j==i){
+        plot.new()
+        text(x = grconvertX(0.5, from = "npc"),
+             y = grconvertY(0.5, from = "npc"),
+             labels = gsub("\\.","\n",sectors[i]),
+             cex=3, font=2, col=c1, srt=0)
+      } else{
+        
+        ula = apply(ll[,c(i,j)],2,rank)/(nrow(ll)+1)
+        fla = apply(-1/log(ula),1,min)
+        u_fla = quantile(fla, probs=c(0.025,0.975))
+        
+        par(pty="s", xaxt="n", yaxt="n")
+        tcplot(fla, u_fla, nt=25, pscale=TRUE, which=2, vci=FALSE, cilty=2, 
+               type="l", ylim=c(0,1.2), ylab="", xlab="", lwd=2)
+        abline(h=1, col=c2, lty=3)
+        axis(side=1, labels = FALSE)
+        axis(side=2, labels = FALSE)
+        
+      }
+    }
+    
+  }
+}
+
+dev.off()
+
+# Plot Technology vs Other Sectors
+jpeg(
+  file = "~/Documents/GitHub/PDM_2022/figures/etaplot_tech.jpeg",
+  width = size, height = size, quality = 100, res = 100
+)
+
+par(mfrow=c(3,4), pty="s")
+
+for (i in c(6)){
+  for (j in seq(1,11)){
+    if (j!=6){
+      
+      ula = apply(ll[,c(i,j)],2,rank)/(nrow(ll)+1)
+      fla = apply(-1/log(ula),1,min)
+      u_fla = quantile(fla, probs=c(0.025,0.975))
+      
+      par(pty="s")
+      tcplot(fla, u_fla, nt=25, pscale=TRUE, which=2, vci=FALSE, cilty=2, 
+             type="l", ylim=c(0,1.2), ylab="Eta", xlab="Quantile", lwd=2)
+      abline(h=1, col=c2, lty=3)
+      title(main=paste(sectors[i],sectors[j],sep="/"), cex.main=2)
+      
+    }
+  }
+}
+
+dev.off()
