47d529a959d3a579f7082af3eb9dba85f0cb51ac
MegaCookies88 <nouira.mongi@gmail.com>
2022-05-26 15:13:47 +0200

    Update skmeans.R

--- R_Scripts/skmeans.R
+++ R_Scripts/skmeans.R
@@ -7,9 +7,30 @@ df = read.csv("~/Documents/GitHub/PDM_2022/data/data1.csv")
 ll = -100*sapply(df[seq(2,ncol(df))], function(x) log(x[-1])-log(x[-length(df$Date)]))
 ll = data.frame(ll)
 
-# Sectors
-sectors = sapply(colnames(ll)[1:12], function(x) substring(x,5))
-sectors = as.vector(sectors)
+### Keeping Market ###
+
+# Standardization
+ll = apply(ll, 2, function(x) 1/(1-rank(x)/(length(x)+1)))
+
+# Angular Measure
+r = apply(ll, 1, function(x) norm(as.matrix(x), type="2"))
+w = ll/r
+
+# Parameters : Threshold / Number of clusters
+r0 = quantile(r,0.95)
+k = 7
+
+# Clustering
+set.seed(1234)
+skm <- skmeans(w[r>r0,], k=k, method=NULL, m=1, weights=1)
+labels = skm$cluster
+centers = skm$prototypes
+write.csv(centers, paste("~/Documents/GitHub/PDM_2022/data/skmeans",k,"_centers_market.csv",sep=""))
+write.csv(labels, paste("~/Documents/GitHub/PDM_2022/data/skmeans",k,"_labels_market.csv",sep=""))
+
+
+### Removing Market ###
+ll = ll[,2:12]
 
 # Standardization
 ll = apply(ll, 2, function(x) 1/(1-rank(x)/(length(x)+1)))
